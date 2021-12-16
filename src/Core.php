5a3199ec1eed93d698da72422f042a56cf755a25
Kevin Dees <kvdees@gmail.com>
2021-12-16 11:56:52 -0500

    initial commit

---
+++ src/Core.php
@@ -0,0 +1,84 @@
+<?php
+namespace ImgixWp;
+
+class Core
+{
+    protected static array $options = [
+        'admin' => false,
+        'imgix_host' => null,
+        'webp' => true,
+        'ext_replace' => true,
+    ];
+
+    /**
+     * @param array $options
+     *
+     * - admin (bool)             - Default is `false`, set to `true` to allow in admin
+     *
+     * - imgix_host (null|string) - Default is `null`, this is to be set to your imgix
+     *                              source URL
+     *
+     * - webp (bool)              - Default is `true`, this will rewrite image URLs as
+     *                              .webp even when imgix is not used
+     *
+     * - ext_replace (bool)       - Default is `true`, this will replace image extensions
+     *                              with .webp but when `false` will append .webp
+     *
+     * @return void
+     */
+    public static function init(array $options = [])
+    {
+        static::$options = array_merge(static::$options, $options, array_filter([
+            'imgix_host' => defined('IMGIX_WP_IMGIX_HOST') ? constant('IMGIX_WP_IMGIX_HOST') : null,
+            'webp' => defined('IMGIX_WP_WEBP') ? constant('IMGIX_WP_WEBP') : null,
+            'ext_replace' => defined('IMGIX_WP_EXT_REPLACE') ? constant('IMGIX_WP_EXT_REPLACE') : null,
+        ]));
+
+        if(is_admin()) {
+            if(static::$options['admin'] === false) {
+                return;
+            }
+        }
+
+        add_filter( 'wp_get_attachment_image_src', static::class . '::filter_wp_get_attachment_image_src');
+        add_filter( 'wp_calculate_image_srcset',  static::class . '::filter_wp_calculate_image_srcset');
+    }
+
+    public static function imgixHost() : string
+    {
+        return trim((string) static::$options['imgix_host'], "/\ \t\n\r\0\x0B");
+    }
+
+    public static function maybeReplaceExtensionWithWebp() : bool
+    {
+        return (bool) static::$options['ext_replace'];
+    }
+
+    public static function maybeWebpUrls() : bool
+    {
+        return (bool) static::$options['webp'];
+    }
+
+    public static function filter_wp_get_attachment_image_src($image)
+    {
+        if(!empty($image) && is_array($image)) {
+            $image[0] = Image::transformImageUrl( (string) $image[0]);
+        }
+
+        return $image;
+    }
+
+    public static function filter_wp_calculate_image_srcset($sources)
+    {
+        if(!empty($sources) && is_array($sources)) {
+            foreach ( $sources as $i => $source ) {
+                if(!empty($source) && is_array($source)) {
+                    $source['url'] = Image::transformImageUrl((string) $source['url']);
+                    $sources[$i] = $source;
+                }
+            }
+        }
+
+        return $sources;
+    }
+}
---
+++ src/Image.php
@@ -0,0 +1,185 @@
+<?php
+namespace ImgixWp;
+
+class Image
+{
+    public static function transformImageUrl(string $url) : string
+    {
+        if(!empty($url)) {
+            if(Core::imgixHost()) {
+                $url = \ImgixWp\Image::transformWordPressUploadsUrlToImgixHost($url);
+            } elseif(Core::maybeWebpUrls() && !Core::maybeReplaceExtensionWithWebp()) {
+                $url = $url . '.webp';
+            } elseif(Core::maybeWebpUrls() && Core::maybeReplaceExtensionWithWebp()) {
+                $url = substr($url, 0, -strlen(strrchr($url, '.'))) . '.webp';
+            }
+        }
+
+        return $url;
+    }
+
+    public static function transformWordPressUploadsUrlToImgixHost($url, string $folder = 'uploads') : string
+    {
+        $host = Core::imgixHost();
+        $url = str_replace(WP_CONTENT_URL . '/' . $folder, 'https://' . $host, $url);
+
+        return $url . '?auto=format';
+    }
+
+    /**
+     * @param string $size         Get media size such as `full` and `thumbnail`. Set preferred imgix size
+     *                             by delimiting with `:`. In example, 'large:full' imgix is `full`.
+     * @param callable|null $cb
+     *
+     * @return mixed|null
+     */
+    public static function imgSize(string $size = 'full', ?callable $cb = null)
+    {
+        [$size_normal, $size_imgix] = array_pad(explode(':', $size, 2), 2, null);
+
+        if(Core::imgixHost()) {
+            $size = $size_imgix ?? $size_normal ?? null;
+        } else {
+            $size = $size_normal;
+        }
+
+        if(is_callable($cb)) {
+            return $cb($size, $size_imgix);
+        }
+
+        return $size ?? null;
+    }
+
+    public static function getSrc($id, $size = 'thumbnail', $icon = false, array $query = [])
+    {
+        if(!$id) { return ''; }
+
+        $size = static::imgSize($size);
+
+        if(!Core::imgixHost()) {
+            return wp_get_attachment_image_src($id, $size, $icon);
+        }
+
+        if(!$src = wp_get_attachment_image_src($id, $size)) { return $src; }
+
+        if(Core::imgixHost() && strpos($size, '?')) {
+            parse_str(substr($size, strpos($size, '?') + 1), $s_query);
+            $query = array_merge($s_query, $query);
+        }
+
+        $query_str = [];
+        foreach ($query as $k => $v) { $query_str[] = $k . '=' . $v; }
+        $query_str = implode('&', $query_str);
+
+        $op = '?';
+        if(strpos($src[0], '?')) { $op = '&'; }
+
+        return [$src[0] . $op . $query_str, $src[1], $src[2], $src[3] ?? false];
+    }
+
+    public static function getImage($id, $size, $icon = false, array $attr = [], array $query = [])
+    {
+        if(!$id) { return ''; }
+
+        $size = static::imgSize($size);
+
+        if(!Core::imgixHost() || empty($query)) {
+            return wp_get_attachment_image($id, $size, $icon, $attr);
+        }
+
+        if(Core::imgixHost() && strpos($size, '?')) {
+            parse_str(substr($size, strpos($size, '?') + 1), $s_query);
+            $query = array_merge($s_query, $query);
+        }
+
+        $query_str = [];
+        $srcset = true;
+        $attr_from_query = [];
+        $scale_up = true;
+        $crop = false;
+        foreach ($query as $k => $v) {
+            $query_str[] = $k . '=' . $v;
+
+            if(in_array($k, ['h', 'w']) && $v < '400') {
+                $srcset = false;
+            }
+
+            if($k === 'fit' && in_array($v, ['max', 'fillmax', 'min'])) { $scale_up = false; }
+            elseif($k === 'fit' && $v === 'crop') { $crop = true; }
+
+            if($k === 'h') { $attr_from_query['height'] = $v; }
+            elseif($k === 'w') { $attr_from_query['width'] = $v; }
+        }
+        $query_str = implode('&', $query_str);
+        [$src, $width, $height] = wp_get_attachment_image_src($id, $size, $icon);
+        $op = '?';
+
+        $h = intval($attr_from_query['height'] ?? 0);
+        $w = intval($attr_from_query['width'] ?? 0);
+
+        if($scale_up) {
+            if($h && $height < $h ) { $h = $height; }
+            if($w && $width < $w) { $w = $width; }
+        }
+
+        if($h && !$w) {
+            $w = (int) ($h / $height * $width);
+        }
+
+        if($w && !$h) {
+            $h = (int) ($w / $width * $height);
+        }
+
+        if($h && $height > $width && !$crop) {
+            $attr_from_query['height'] = $h;
+            $attr_from_query['width'] = (int) ($h / $height * $width);
+        }
+
+        if($w && $width > $height && !$crop) {
+            $attr_from_query['width'] = $w;
+            $attr_from_query['height'] = (int) ($w / $width * $height);
+        }
+
+        if($w && $h && $width === $height) {
+            $attr_from_query['width'] = $w;
+            $attr_from_query['height'] = $h;
+        }
+
+        if(strpos($src, '?')) { $op = '&'; }
+
+        $srcset_cb = function($sources) use ($query_str) {
+            if(!empty($sources) && is_array($sources)) {
+                foreach ( $sources as $i => $source ) {
+
+                    $op = '?';
+                    if(strpos($source['url'], '?')) { $op = '&'; }
+
+                    $source['url'] = $source['url'] . $op . $query_str;
+
+                    $sources[$i] = $source;
+                }
+            }
+
+            return $sources;
+        };
+
+        $priority = 678992;
+
+        if($srcset) {
+            add_filter( 'wp_calculate_image_srcset', $srcset_cb, $priority);
+        } else {
+            add_filter( 'wp_get_attachment_metadata', '__return_null', $priority);
+        }
+
+        $attr = array_merge($attr, $attr_from_query, ['src' => $src . $op . $query_str]);
+        $img = wp_get_attachment_image($id, $size, $icon, $attr);
+
+        if($srcset) {
+            remove_filter('wp_calculate_image_srcset', $srcset_cb, $priority);
+        } else {
+            remove_filter( 'wp_get_attachment_metadata', '__return_null', $priority);
+        }
+
+        return $img;
+    }
+}
